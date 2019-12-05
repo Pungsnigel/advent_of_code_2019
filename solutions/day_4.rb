b9c402c77063bfe9519fce2676d181d4be35871c
Jesper Lugner <jesper.lugner@icloud.com>
2019-12-05 17:41:44 +0100

    Add day 4

--- solutions/day_4.rb
+++ solutions/day_4.rb
@@ -1,7 +1,40 @@
 # frozen_string_literal: true
 
 module Day4
-  def self.problem_a(changes) end
+  class << self
+    def problem_a(range)
+      start, stop = range.first.split("-").map(&:to_i)
+      (start..stop).count { |attempt| valid_password?(attempt) }
+    end
 
-  def self.problem_b(changes) end
+    def problem_b(range)
+      start, stop = range.first.split("-").map(&:to_i)
+      (start..stop).count { |attempt| valid_password?(attempt, false) }
+    end
+      
+    def valid_password?(attempt, allow_triplets = true)
+      numbers = attempt.to_s.chars.map(&:to_i)
+      return false unless has_adjecent_duplicates?(numbers, allow_triplets)
+      return false if has_decreases?(numbers)
+      true
+    end
+
+    def has_adjecent_duplicates?(numbers, allow_triplets)
+      numbers.each_with_index do |n, i|
+        if allow_triplets
+          return true if numbers[i + 1] == n
+        else
+          return true if numbers[i + 1] == n && numbers[i + 2] != n && numbers[i - 1] != n
+        end
+      end
+      false
+    end
+
+    def has_decreases?(numbers)
+      numbers.each_with_index do |n, i|
+        return true if numbers[i + 1] && numbers[i + 1] < n
+      end
+      false
+    end
+  end
 end
--- test/day_4_tests.rb
+++ test/day_4_tests.rb
@@ -3,7 +3,21 @@
 require 'minitest/autorun'
 require_relative('../solutions/day_4')
 class Day4Tests < Minitest::Test
-  def test_day_4_solution_a; end
+  def test_day_4_solution_a
+    assert Day4.valid_password?(111_111)
+    assert !Day4.valid_password?(223_450)
+    assert !Day4.valid_password?(123_789)
+    assert !Day4.valid_password?(500_000)
+    assert !Day4.valid_password?(123_456)
+    assert Day4.valid_password?(123_345)
+  end
 
-  def test_day_4_solution_b; end
+  def test_day_4_solution_b
+    assert !Day4.valid_password?(111_111, false)
+    assert !Day4.valid_password?(223_450, false)
+    assert !Day4.valid_password?(123_789, false)
+    assert !Day4.valid_password?(500_000, false)
+    assert !Day4.valid_password?(123_456, false)
+    assert Day4.valid_password?(123_345, false)
+  end
 end
