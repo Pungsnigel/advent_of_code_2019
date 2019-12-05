183f90da87cac292c70f456db1313afd645950b8
Jesper Lugner <jesper.lugner@icloud.com>
2019-12-05 14:59:28 +0100

    Solve 2b

--- solutions/day_2.rb
+++ solutions/day_2.rb
@@ -3,24 +3,39 @@ require 'yaml'
 module Day2
   def self.problem_a(program)
     program = program.first.split(',').map(&:to_i)
-    program[1] = 12
-    program[2] = 2
-    run_program(program)
+    computer = IntcodeComputer.new(program, 12, 2)
+    computer.compute
   end
 
-  def self.problem_b(changes) end
+  def self.problem_b(program)
+    program = program.first.split(',').map(&:to_i)
+    100.times do |noun|
+      100.times do |verb|
+        computer = IntcodeComputer.new(program.dup, noun, verb)
+        return 100 * noun + verb if computer.compute == 196_907_20
+      end
+    end
+  end
 
-  def self.run_program(program)
-    program.each_slice(4) do |instruction|
-      operation, value_slot_a, value_slot_b, target_slot = instruction
-      return program if operation == 99
-      raise 'Unkown operation' unless [1, 2].include? operation
-
-      value_a = program[value_slot_a]
-      value_b = program[value_slot_b]
-      program[target_slot] = operation == 1 ? value_a + value_b : value_a * value_b
+  class IntcodeComputer
+    def initialize(program, noun = nil, verb = nil)
+      program[1] = noun if noun
+      program[2] = verb if verb
+      @program = program
     end
 
-    raise 'No end of operation'
+    def compute
+      @program.each_slice(4) do |instruction|
+        operation, value_slot_a, value_slot_b, target_slot = instruction
+        return @program[0] if operation == 99
+        raise 'Unkown operation' unless [1, 2].include? operation
+  
+        value_a = @program[value_slot_a]
+        value_b = @program[value_slot_b]
+        @program[target_slot] = operation == 1 ? value_a + value_b : value_a * value_b
+      end
+  
+      raise 'No end of operation'
+    end
   end
 end
--- test/day_2_tests.rb
+++ test/day_2_tests.rb
@@ -5,17 +5,11 @@ require_relative('../solutions/day_2')
 
 class Day2Tests < Minitest::Test
   def test_day_2_solution_a
-    assert_equal(
-      [3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50],
-      Day2.run_program([1,9,10,3,2,3,11,0,99,30,40,50])
-    )
-    assert_equal([2, 0, 0, 0, 99], Day2.run_program([1, 0, 0, 0, 99]))
-    assert_equal([2, 3, 0, 6, 99], Day2.run_program([2, 3, 0, 3, 99]))
-    assert_equal([2, 4, 4, 5, 99, 9801], Day2.run_program([2, 4, 4, 5, 99, 0]))
-    assert_equal(
-      [30, 1, 1, 4, 2, 5, 6, 0, 99],
-      Day2.run_program([1, 1, 1, 4, 99, 5, 6, 0, 99])
-    )
+    assert_equal(3500, Day2::IntcodeComputer.new([1,9,10,3,2,3,11,0,99,30,40,50]).compute)
+    assert_equal(2, Day2::IntcodeComputer.new([1, 0, 0, 0, 99]).compute)
+    assert_equal(2, Day2::IntcodeComputer.new([2, 3, 0, 3, 99]).compute)
+    assert_equal(2, Day2::IntcodeComputer.new([2, 4, 4, 5, 99, 0]).compute)
+    assert_equal(30, Day2::IntcodeComputer.new([1, 1, 1, 4, 99, 5, 6, 0, 99]).compute)
   end
 
   def test_day_2_solution_b; end
